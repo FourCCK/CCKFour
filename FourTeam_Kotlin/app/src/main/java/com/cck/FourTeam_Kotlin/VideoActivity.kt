5b43461ef393df815783ff7f2af8ed528e3e5113
guoyongzhen <168167886@qq.com>
2017-12-28 20:40:56 +0800

    z

--- FourTeam_Kotlin/app/src/main/java/com/cck/FourTeam_Kotlin/VideoActivity.kt
+++ FourTeam_Kotlin/app/src/main/java/com/cck/FourTeam_Kotlin/VideoActivity.kt
@@ -12,14 +12,19 @@ class VideoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_video)
+
+
        val ijk=findViewById(R.id.ijk) as IjkVideoView
         //intent传值
         val stringExtra = intent.getStringExtra("url")
         val tltie = intent.getStringExtra("title")
         val description = intent.getStringExtra("description")
         //视频播放
+
+
         val androidMediaController = AndroidMediaController(this, false)
         ijk.setMediaController(androidMediaController)
+
         //播放路径
         ijk.setVideoURI(Uri.parse(stringExtra))
         ijk.start()
