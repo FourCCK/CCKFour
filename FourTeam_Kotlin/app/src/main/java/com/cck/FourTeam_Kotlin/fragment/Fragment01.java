54d89ce28b406158d96ad6164d28a2683e6b6be4
YourFace1 <1427581626@qq.com>
2017-12-28 18:48:50 +0800

    eeeeee

--- FourTeam_Kotlin/app/src/main/java/com/cck/FourTeam_Kotlin/fragment/Fragment01.java
+++ FourTeam_Kotlin/app/src/main/java/com/cck/FourTeam_Kotlin/fragment/Fragment01.java
@@ -6,9 +6,9 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.cck.FourTeam_Kotlin.R;
-
 
 /**
  * Created by C-PC on 2017/12/21.
@@ -19,7 +19,6 @@ public class Fragment01 extends Fragment{
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment01,null);
-
 
 
 
